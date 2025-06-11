f1c06bfbe7dfd366cdee230e5d33ad09ebd9828b
sita1013 <seluvaiasariahita@gmail.com>
2025-06-11 09:55:07 +0100

    temp converter

--- calc_weight_exercise.py
+++ calc_weight_exercise.py
@@ -18,6 +18,7 @@ else:
 print(f"The result is {round(result, 3)}.")
 """
 
+"""
 #weight converter
 weight = float(input("Enter your weight: "))
 unit = input("Kilograms or Pounds? (K or L): ")
@@ -32,5 +33,18 @@ elif unit == "L":
     print(f"Your weight is {round(weight, 1)} {unit}.")
 else: 
     print(f"{unit} was not a valid input.")
+"""
+#temp converter
+import math 
 
+unit = input ("Is this temperature in Celsius or Farenheit? (C/F): ")
+temp = float(input("Enter the temperature: "))
 
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temperature in Fahrenheit is: {temp}°F.")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp}°C.")
+else: 
+    print(f"{unit} is an invalid unit of measurement.")
