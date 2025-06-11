a7456adb5e90abedb2a7421dff85a85dc697689a
sita1013 <seluvaiasariahita@gmail.com>
2025-06-11 09:30:42 +0100

    if statements

---
+++ string_functions.py
@@ -0,0 +1,35 @@
+"""#if statement- do some code IF the condition is True
+
+age = int(input("Enter your age: "))
+
+if age >= 18 and age <= 150: 
+    print("You are now signed up.")
+elif age <= 17 and age > 0: 
+    print("You are not old enough.")
+else: 
+    print("You're funny...nice try.")
+"""
+"""
+response = input("Would you like food? (y/n): ")
+if response == "y": 
+    print("Okay, I'll bring some food.")
+elif response == "n": 
+    print("No food for you.")
+else: 
+    print("invalid input")
+"""
+
+#boolean
+"""
+for_sale = False
+if for_sale: 
+    print("This item is for sale.")
+else: 
+    print("This item is not for sale.")
+"""
+
+online = True
+if online: 
+    print("The user is online.")
+else: 
+    print("The user is offline.")
