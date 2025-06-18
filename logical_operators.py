5ed98740908599619ffaed17cebb7adfc3bd536e
sita1013 <seluvaiasariahita@gmail.com>
2025-06-18 13:55:25 +0100

    conditional statements

--- logical_operators.py
+++ logical_operators.py
@@ -9,7 +9,7 @@ else:
     print("The outdoor even is still scheduled.")
 """
 
-""""""
+"""
 temp = 0
 is_sunny = False
 
@@ -21,4 +21,19 @@ elif temp <= 0 and not is_sunny:
     print("It is cloudy.")
 else:
     print("Something else.")
+"""
+#conditional expression
+"""
+num = -5
+print("Positive" if num > 0 else "Negative")
 
+
+num = 6
+result = "Even" if num % 2 == 0 else "Odd"
+print(result)
+"""
+
+user_role = "guest"
+
+access_level = "Full Access" if user_role == "admin" else "Limited Acess"
+print(access_level)
